4e15255ea4df9a41a60ed3737eb9cc1d5bf97ba7
Marat Safin <jeizsm@gmail.com>
2018-06-15 06:58:13 +0300

    add setter to ReplyKeyboardRemove

--- src/objects.rs
+++ src/objects.rs
@@ -273,7 +273,7 @@ pub struct KeyboardButton {
 /// keyboard and display the default letter-keyboard. By default, custom keyboards are displayed
 /// until a new keyboard is sent by a bot. An exception is made for one-time keyboards that are
 /// hidden immediately after the user presses a button (see ReplyKeyboardMarkup).
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(setter, Serialize, Deserialize, Debug)]
 pub struct ReplyKeyboardRemove {
     pub remove_keyboard: bool,
     #[serde(skip_serializing_if = "Option::is_none")]
